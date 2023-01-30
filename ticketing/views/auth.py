e4af1788791901e993f085b1a4327487df07eacd
Ka Chun Chong <ka.c.chong@kcl.ac.uk>
2023-01-30 15:35:50 +0000

    Restrict access to SignupView if logged in

--- ticketing/views/auth.py
+++ ticketing/views/auth.py
@@ -7,12 +7,18 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from ticketing.forms import SignupForm, LoginForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 class SignupView(SuccessMessageMixin, CreateView):
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
     form_class = SignupForm
     success_message = "Your account was created successfully"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('home'))
+        return super().get(request, *args, **kwargs)
 
 class CustomLoginView(LoginView):
     def get_success_url(self):
