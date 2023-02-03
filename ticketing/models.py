980ba241c144628088c2c1fbdfd7f0a201a1663e
Ka Chun Chong <ka.c.chong@kcl.ac.uk>
2023-02-03 02:01:54 +0000

    Add create specialist and director to UserManager

--- ticketing/models.py
+++ ticketing/models.py
@@ -16,6 +16,35 @@ class CustomUserManager(UserManager):
         user.role = 'ST'
         user.save(using=self._db)
         return user
+
+    def create_specialist(self, email, password=None, first_name=None, last_name=None, **extra_fields):
+        if not email:
+            raise ValueError("Enter an email address")
+        if not first_name:
+            raise ValueError("Enter a first name")
+        if not last_name:
+            raise ValueError("Enter a last name")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.role = 'SP'
+        user.save(using=self._db)
+        return user
+    
+    def create_director(self, email, password=None, first_name=None, last_name=None, **extra_fields):
+        if not email:
+            raise ValueError("Enter an email address")
+        if not first_name:
+            raise ValueError("Enter a first name")
+        if not last_name:
+            raise ValueError("Enter a last name")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.role = 'DI'
+        user.save(using=self._db)
+        return user
+    
         
     def create_superuser(self, email, password, first_name, last_name):
         user = self.create_user(email, password=password, first_name=first_name, last_name=last_name)
