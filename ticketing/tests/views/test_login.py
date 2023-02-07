28c59af98ae4e0c4a658bf5a09e6e538d024b960
Tsz Shun Max Chang <maxc22123@gmail.com>
2023-02-07 14:27:48 +0000

    Change test case for student login

--- ticketing/tests/views/test_login.py
+++ ticketing/tests/views/test_login.py
@@ -72,9 +72,9 @@ class LoginViewTestCase(TestCase, LogInTester):
         form_input = { 'username': 'johndoe@example.com', 'password': 'Password123'}
         response = self.client.post(self.url, form_input, follow=True)
         self.assertTrue(self._is_logged_in())
-        response_url = reverse('home')
+        response_url = reverse('student_dashboard')
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'student_dashboard.html')
 
     def test_successful_specialist_log_in(self):
         form_input = { 'username': 'janedoe@example.com', 'password': 'Password123'}
