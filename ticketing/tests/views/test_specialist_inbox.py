5bedba59a4741e896ef54146e42c2399ac9bad58
Adam-Moujar <amoujarbakhti@gmail.com>
2023-02-13 14:04:36 +0000

    Test for specialist inbox

--- ticketing/tests/views/test_specialist_inbox.py
+++ ticketing/tests/views/test_specialist_inbox.py
@@ -6,13 +6,52 @@ from ticketing.models.users import User
 class SpecialistInboxViewTestCase(TestCase):
     def setUp(self):
         self.url = reverse('specialist_dashboard')
-        User.objects.create_user(
+        self.specialist = User.objects.create_specialist(
             email='johndoe@example.com',
             first_name='John',
             last_name='Doe',
             password='Password@123',
-            role='SP',
         )
 
-    def test_adult_hub_url(self):
+        self.student = User.objects.create_user(
+            email='janedoe@example.com',
+            first_name='Jane',
+            last_name='Doe',
+            password='Password@123',
+        )
+
+        self.director = User.objects.create_director(
+            email='bobdoe@example.com',
+            first_name='Bob',
+            last_name='Doe',
+            password='Password@123',
+        )
+
+    def test_specialist_dashboard_url(self):
         self.assertEqual(self.url, '/specialist_dashboard/')
+
+    def test_get_specialist_dashboard_as_student(self):
+        self.client = Client()
+        loggedin = self.client.login(
+            email=self.student.email, password='Password@123'
+        )
+
+        self.assertTrue(loggedin)
+        response = self.client.get(self.url, follow=True)
+        response_url = reverse('home')
+        self.assertRedirects(
+            response, response_url, status_code=302, target_status_code=200
+        )
+
+    def test_get_specialist_dashboard_as_director(self):
+        self.client = Client()
+        loggedin = self.client.login(
+            email=self.director.email, password='Password@123'
+        )
+
+        self.assertTrue(loggedin)
+        response = self.client.get(self.url, follow=True)
+        response_url = reverse('home')
+        self.assertRedirects(
+            response, response_url, status_code=302, target_status_code=200
+        )
--- ticketing/views/specialist_inbox.py
+++ ticketing/views/specialist_inbox.py
@@ -8,60 +8,74 @@ from ticketing.decorators import roles_allowed
 
 from ticketing.models import Ticket, SpecialistInbox, SpecialistDepartment
 
-@method_decorator(roles_allowed(allowed_roles = ['SP', 'DI']), name='dispatch')
+
+@method_decorator(roles_allowed(allowed_roles=['SP']), name='dispatch')
 @method_decorator(login_required, name='dispatch')
 class SpecialistInboxView(ListView):
     model = Ticket
     template_name = 'specialist_dashboard.html'
 
     def get_queryset(self):
-        user            = self.request.user
-        user_department = SpecialistDepartment.objects.filter(specialist = user)
+        user = self.request.user
+        user_department = SpecialistDepartment.objects.filter(specialist=user)
 
         ticket_list = []
-        if self.request.method == "POST":
+        if self.request.method == 'POST':
             ticket_type = self.request.POST.get('type_of_ticket')
             ticket_list = self.get_tickets(user, user_department, ticket_type)
 
-        if self.request.method == "GET":
-            ticket_type = "personal"
+        if self.request.method == 'GET':
+            ticket_type = 'personal'
             ticket_list = self.get_tickets(user, user_department, ticket_type)
-        
+
         return ticket_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ticket_type = ""
+        ticket_type = ''
 
-        if self.request.method == "POST":
+        if self.request.method == 'POST':
             ticket_type = self.request.POST.get('type_of_ticket')
 
-        if self.request.method == "GET":
-            ticket_type = "personal"
+        if self.request.method == 'GET':
+            ticket_type = 'personal'
 
-        context["ticket_type"] = ticket_type
+        context['ticket_type'] = ticket_type
         return context
 
     def post(self, request, *args, **kwargs):
-        return render(request, 'specialist_dashboard.html', {
-            'object_list': self.get_queryset(),
-            "ticket_type": self.request.POST.get('type_of_ticket')
-            })
+        return render(
+            request,
+            'specialist_dashboard.html',
+            {
+                'object_list': self.get_queryset(),
+                'ticket_type': self.request.POST.get('type_of_ticket'),
+            },
+        )
 
-    def get_tickets(self,user, user_department, ticket_type):
+    def get_tickets(self, user, user_department, ticket_type):
         match ticket_type:
-            case "department": 
-                if (len(user_department) != 0):
-                    full_ticket_list = Ticket.objects.filter(department = user_department.first().department)
+            case 'department':
+                if len(user_department) != 0:
+                    full_ticket_list = Ticket.objects.filter(
+                        department=user_department.first().department
+                    )
                     ticket_list = []
                     for ticket in full_ticket_list:
-                        if len(SpecialistInbox.objects.filter(ticket = ticket)) == 0:
-                            ticket_list.append(ticket) 
-                        
-            case "personal" : ticket_list = Ticket.objects.filter(
-                id__in = SpecialistInbox.objects.filter(specialist = user).values_list('ticket_id', flat = True)
-                ) 
+                        if (
+                            len(SpecialistInbox.objects.filter(ticket=ticket))
+                            == 0
+                        ):
+                            ticket_list.append(ticket)
 
-            case default : ticket_list = []
+            case 'personal':
+                ticket_list = Ticket.objects.filter(
+                    id__in=SpecialistInbox.objects.filter(
+                        specialist=user
+                    ).values_list('ticket_id', flat=True)
+                )
 
-        return ticket_list      
+            case default:
+                ticket_list = []
+
+        return ticket_list
