33c4d1fd85dfb23e41702a0dc1972b9d0e363c37
Vaibhavkumar Patel <vaibhav.patel.kumar@gmail.com>
2023-03-17 11:39:20 +0000

    Recreate Fixtures and Refactor search bar

--- ticketing/views/search_bar.py
+++ ticketing/views/search_bar.py
@@ -31,65 +31,40 @@ class SearchBarView(TemplateView):
             context['top_FAQs'] = faqs
         return context
     
-    # helper functions
+    def rank_items(self, query, model, queryset, result_size, faq_rank = False):
+        items = queryset.all()
+        if(not faq_rank):
+            item_names = [item.name for item in items]
+        else: 
+            item_names = [item.questions for item in items]
+        item_dict = {}
+        item_name_chunks = [item_names[i:i+10] for i in range(0, len(item_names), 10)]
+        for item_name_chunk in item_name_chunks:
+            data = model.get_data(query, item_name_chunk)
+            for i in range(0, len(data['scores'])):
+                item_name = data['labels'][i]
+                item_dict[item_name] = data['scores'][i]
+        item_dict = dict(sorted(item_dict.items(), key=lambda x: x[1], reverse=True)[:result_size])
+        if(not faq_rank):
+            item_dict = {key: items.get(name=key).id for key in item_dict.keys()}
+        else:
+            item_dict = {key: items.get(questions=key).answer for key in item_dict.keys()}
+        print(item_dict)
+        return item_dict
+
     def rank_departments(self, query, result_size: int):
         departments = Department.objects.all()
-        department_names = [department.name for department in departments]
-        department_dict = {}
-        # Split the department names into chunks of size 10
-        department_name_chunks = [department_names[i:i+10] for i in range(0, len(department_names), 10)]
-        for department_name_chunk in department_name_chunks:
-            data = ml_api.get_data(query, department_name_chunk)
+        return self.rank_items(query, ml_api, departments, result_size)
 
-            # use the name and scores from zero shot classifier to upddate dictionary
-            for i in range (0, len(data['scores'])):
-                department_name = data['labels'][i]
-                department_dict[department_name] = data['scores'][i]
-
-        # Sort the departments by score and return the top 3, format {department_names : score}
-        department_dict = dict(sorted(department_dict.items(), key=lambda x: x[1], reverse=True)[:result_size])
-        
-        # change values to ids -> department_dict {names : ids}
-        department_dict = {key: departments.get(name=key).id for key in department_dict.keys()}
-        return department_dict
-
-    def rank_subsections(self, query, department_id : int, result_size : int):
-        department_obj = Department.objects.get(id = department_id)
+    def rank_subsections(self, query, department_id: int, result_size: int):
+        department_obj = Department.objects.get(id=department_id)
         subsections = Subsection.objects.filter(department=department_obj)
-        subsection_names = [subsection.name for subsection in subsections]
-        subsection_dict = {}
-        # Split the subsections names into chunks of size 10
-        subsection_name_chunks = [subsection_names[i:i+10] for i in range(0, len(subsection_names), 10)]
-        for subsection_name_chunk in subsection_name_chunks:
-            data = ml_api.get_data(query, subsection_name_chunk)
-            # use the name and scores from zero shot classifier to upddate dictionary
-            for i in range (0, len(data['scores'])):
-                subsection_name = data['labels'][i]
-                subsection_dict[subsection_name] = data['scores'][i]
-        # Sort the subsections by score and return the top 3, format {names : score}        
-        subsection_dict = dict(sorted(subsection_dict.items(), key=lambda x: x[1], reverse=True)[:result_size])
+        return self.rank_items(query, ml_api, subsections, result_size)
 
-        # change values to ids -> subsections_dict {names : ids}
-        subsection_dict = {key : subsections.get(name=key).id for key in subsection_dict.keys()}
-        return subsection_dict
-    
-    def rank_faqs(self, query, subsection_id : int, result_size: int):
+    def rank_faqs(self, query, subsection_id: int, result_size: int):
         faqs = FAQ.objects.filter(subsection_id=subsection_id)
         faq_questions = [faq.questions for faq in faqs]
-        faqs_dict = {}
-        # Split the faq names into chunks of size 10
-        faq_question_chunks = [faq_questions[i:i+10] for i in range(0, len(faq_questions), 10)]
-        for faq_question_chunk in faq_question_chunks:
-            data = ml_api.get_data(query, faq_question_chunk)
-            # use the name and scores from zero shot classifier to upddate dictionary
-            for i in range (0, len(data['scores'])):
-                faq_question = data['labels'][i]
-                faqs_dict[faq_question] = data['scores'][i]
+        return self.rank_items(query, ml_api, faqs, result_size, faq_rank=True)
 
-        # Sort the FAQs by score and return the top 5, format {question : score}          
-        faqs_dict = dict(sorted(faqs_dict.items(), key=lambda x: x[1], reverse=True)[:result_size])
-        
-        # change the value to answers -> Faq_dict {question : answer}
-        faqs_dict = {questions : faqs.get(questions=questions).answer for questions in faqs_dict.keys() }
-        return faqs_dict
+
 
