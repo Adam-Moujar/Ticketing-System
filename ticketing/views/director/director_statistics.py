a83237b6ed395ba5084cefb769c84afb433de4b7
Harry Sharma <harrysharma1066@gmail.com>
2023-03-23 12:51:11 +0000

    Make time bug fixes

--- ticketing/views/director/director_statistics.py
+++ ticketing/views/director/director_statistics.py
@@ -97,7 +97,8 @@ class DirectorStatisticsView(LoginRequiredMixin, RoleRequiredMixin, ListView):
         else:
             # return total_time / counter
             time = total_time / counter
-            hours = time.total_seconds() // 60*60
+            print(time.total_seconds())
+            hours = time.total_seconds() // (60*60)
             mins = (time.total_seconds() - hours*60*60 ) // 60
             secs = (time.total_seconds() - hours*60*60 - mins*60) 
             return f"{hours} hours {mins} mins { round(secs, 1)} secs"
